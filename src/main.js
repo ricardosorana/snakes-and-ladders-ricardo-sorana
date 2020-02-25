14cbf0bc86322ce87cf9b6b9ec65ed6bb00e9d33
Ricardo Sorana <ricardosorana@gmail.com>
2020-02-24 22:36:00 -0300

    V3 incomplete

--- src/main.js
+++ src/main.js
@@ -8,8 +8,14 @@ class Player {
 
 let player1 = new Player('red');
 let player2 = new Player('blue');
+let player3 = new Player('green');
+let player4 = new Player('white');
 const redWinMessage = document.getElementById('red-win-message');
 const blueWinMessage = document.getElementById('blue-win-message');
+const greenWinMessage = document.getElementById('green-win-message');
+const whiteWinMessage = document.getElementById('white-win-message');
+let numPlayers;
+
 let turn = player1;
 let diceNumber;
 
@@ -61,10 +67,43 @@ function movePlayer(randomNumber, player, turn) {
 //============================ MOVE FUNCTION AUX ============================//
 
 function checkTurn() {
-  if(turn == player1) {
-    return turn = player2;
+  if(numPlayers === 2){
+    console.log('teste');
+    if(turn === player1) {
+      console.log('red-blue');
+      return turn = player2;
+    } else {
+      console.log('blue-red');
+      return turn = player1;
+    }
+  } else if (numPlayers === 3) {
+    console.log('teste');
+    if(turn === player1) {
+      console.log('red-blue');
+      return turn = player2;
+    } else if(turn === player2) {
+      console.log('blue-green');
+      return turn = player3;
+    } else {
+      console.log('green-red');
+      return turn = player1;
+    }
   }
-  return turn = player1;
+
+  console.log('teste');
+    if(turn === player1) {
+      console.log('red-blue');
+      return turn = player2;
+    } else if(turn === player2) {
+      console.log('blue-green');
+      return turn = player3;
+    } else if(turn === player3) {
+      console.log('green-white');
+      return turn = player4;
+    } else {
+      console.log('white-red');
+      return turn = player1;
+    }
 }
 
 function checkEndGame(position, turn) {
@@ -72,8 +111,12 @@ function checkEndGame(position, turn) {
   if(position === 63){
     if(turn.color === 'red'){
       redWinMessage.style.display = 'block';
-    } else {
+    } else if(turn.color === 'blue'){
       blueWinMessage.style.display = 'block';      
+    } else if(turn.color === 'green'){
+      greenWinMessage.style.display = 'block';
+    } else {
+      whiteWinMessage.style.display = 'block';
     }
     // startButton.style.display = 'block';
     console.log(`${turn.color} Win!`);
@@ -146,14 +189,21 @@ function checkSnakes(turn, position, player){
 //============================ MOVE FUNCTION AUX ============================//
 
 
-//============================ START ============================//
+//============================ PLAYER CHOOSE ============================//
 
-const startButton = document.getElementById('start-button');
+const twoPlayerButton = document.getElementById('two-players-button');
+const threePlayerButton = document.getElementById('three-players-button');
+const fourPlayerButton = document.getElementById('four-players-button');
 
-startButton.addEventListener('click', () => {
 
-  startButton.style.display = 'none';
-  
+twoPlayerButton.addEventListener('click', () => {
+
+  numPlayers = 2;
+
+  twoPlayerButton.style.display = 'none';
+  threePlayerButton.style.display = 'none';
+  fourPlayerButton.style.display = 'none';
+
   let pawnRed = document.createElement('div');
   pawnRed.id = "red-player";
   document.getElementById('position-1').appendChild(pawnRed);
@@ -167,7 +217,65 @@ startButton.addEventListener('click', () => {
 
 });
 
-//============================ START ============================//
+threePlayerButton.addEventListener('click', () => {
+
+  numPlayers = 3;
+
+  twoPlayerButton.style.display = 'none';
+  threePlayerButton.style.display = 'none';
+  fourPlayerButton.style.display = 'none';
+
+  let pawnRed = document.createElement('div');
+  pawnRed.id = "red-player";
+  document.getElementById('position-1').appendChild(pawnRed);
+  document.getElementById("red-player").addEventListener("click", function(){movePlayer(diceNumber, "red-player", turn)});
+
+
+  let pawnBlue = document.createElement('div');
+  pawnBlue.id = "blue-player";
+  document.getElementById('position-1').appendChild(pawnBlue);
+  document.getElementById("blue-player").addEventListener("click", function(){movePlayer(diceNumber, "blue-player", turn)});
+
+  let pawnGreen = document.createElement('div');
+  pawnGreen.id = "green-player";
+  document.getElementById('position-1').appendChild(pawnGreen);
+  document.getElementById("green-player").addEventListener("click", function(){movePlayer(diceNumber, "green-player", turn)});
+
+});
+
+fourPlayerButton.addEventListener('click', () => {
+
+  numPlayers = 4;
+
+  twoPlayerButton.style.display = 'none';
+  threePlayerButton.style.display = 'none';
+  fourPlayerButton.style.display = 'none';
+
+  let pawnRed = document.createElement('div');
+  pawnRed.id = "red-player";
+  document.getElementById('position-1').appendChild(pawnRed);
+  document.getElementById("red-player").addEventListener("click", function(){movePlayer(diceNumber, "red-player", turn)});
+
+
+  let pawnBlue = document.createElement('div');
+  pawnBlue.id = "blue-player";
+  document.getElementById('position-1').appendChild(pawnBlue);
+  document.getElementById("blue-player").addEventListener("click", function(){movePlayer(diceNumber, "blue-player", turn)});
+
+  let pawnGreen = document.createElement('div');
+  pawnGreen.id = "green-player";
+  document.getElementById('position-1').appendChild(pawnGreen);
+  document.getElementById("green-player").addEventListener("click", function(){movePlayer(diceNumber, "green-player", turn)});
+
+  let pawnWhite = document.createElement('div');
+  pawnWhite.id = "white-player";
+  document.getElementById('position-1').appendChild(pawnWhite);
+  document.getElementById("white-player").addEventListener("click", function(){movePlayer(diceNumber, "white-player", turn)});
+
+});
+
+
+//============================ PLAYER CHOOSE ============================//
 
 
 //============================ DICE FUNCTION ============================//
@@ -198,3 +306,27 @@ function getRandomNumber(min, max) {
 document.getElementById("button").addEventListener("click", rollDice);
 
 //============================ DICE FUNCTION ============================//
+
+
+//============================ START ============================//
+
+// const startButton = document.getElementById('start-button');
+
+// startButton.addEventListener('click', () => {
+
+//   startButton.style.display = 'none';
+  
+//   let pawnRed = document.createElement('div');
+//   pawnRed.id = "red-player";
+//   document.getElementById('position-1').appendChild(pawnRed);
+//   document.getElementById("red-player").addEventListener("click", function(){movePlayer(diceNumber, "red-player", turn)});
+
+
+//   let pawnBlue = document.createElement('div');
+//   pawnBlue.id = "blue-player";
+//   document.getElementById('position-1').appendChild(pawnBlue);
+//   document.getElementById("blue-player").addEventListener("click", function(){movePlayer(diceNumber, "blue-player", turn)});
+
+// });
+
+//============================ START ============================//
